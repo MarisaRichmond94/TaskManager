174a7f7d03c3a4cb8945c9fc50a4024526ed7735
Marisa D. Richmond <marisa.richmond94@gmail.com>
2022-06-04 19:22:43 -0700

    dY2fJMWK: Task List Model
    - Update task model according to ERD changes

--- server/src/main/kotlin/com/marisarichmond/taskmanager/models/TaskList.kt
+++ server/src/main/kotlin/com/marisarichmond/taskmanager/models/TaskList.kt
@@ -5,31 +5,24 @@ import java.util.*
 import javax.persistence.*
 
 @Entity
-@Table(name = "tasks")
-data class Task(
+@Table(name = "task_lists")
+data class TaskList(
     @Id
     val id: UUID = UUID.randomUUID(),
-    val objective: String,
-    val orderIndex: Int,
+    val name: String,
     val description: String? = null,
     @OneToOne
-    @JoinColumn(name = "task_list_id")
-    val taskList: TaskList,
-    @ManyToMany
-    @JoinColumn(name = "tag_id")
-    val tags: Set<Tag>? = setOf(),
+    @JoinColumn(name = "user_id")
+    val user: User,
 ) {
-    override fun toString(): String {
-        return this::class.simpleName +
-            "(id = $id, objective = $objective, orderIndex = $orderIndex, description = $description)"
-    }
+    override fun toString(): String = this::class.simpleName + "(id = $id, name = $name, description = $description)"
 
     override fun hashCode(): Int = super.hashCode()
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
-        other as Task
+        other as TaskList
 
         return id == other.id
     }
